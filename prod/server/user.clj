533ba9e4277e6429148a82b79cf2587b9e84d331
Michael Gough <innovative.engineer@gmail.com>
2017-03-29 21:31:54 -0700

    remove figwheel

--- prod/server/user.clj
+++ prod/server/user.clj
@@ -4,33 +4,9 @@
     [clojure.stacktrace :refer (print-stack-trace)]
     [clojure.tools.namespace.repl :refer [disable-reload! refresh clear set-refresh-dirs]]
     [com.stuartsierra.component :as component]
-    [figwheel-sidecar.system :as fig]
     [app.system :as sys]
     [taoensso.timbre :as timbre]))
 
-;;FIGWHEEL
-(def figwheel (atom nil))
-
-(defn start-figwheel
-  "Start Figwheel on the given builds, or defaults to build-ids in `figwheel-config`."
-  ([]
-   (let [props (System/getProperties)
-         figwheel-config (fig/fetch-config)
-         all-builds (->> figwheel-config :all-builds (mapv :id))]
-     (start-figwheel (keys (select-keys props all-builds)))))
-  ([build-ids]
-   (let [figwheel-config (fig/fetch-config)
-         default-build-ids (-> figwheel-config :data :build-ids)
-         build-ids (if (empty? build-ids) default-build-ids build-ids)
-         preferred-config (assoc-in figwheel-config [:data :build-ids] build-ids)]
-     (reset! figwheel (component/system-map
-                        :figwheel-system (fig/figwheel-system preferred-config)
-                        :css-watcher (fig/css-watcher {:watch-paths ["resources/public/css"]})))
-     (println "STARTING FIGWHEEL ON BUILDS: " build-ids)
-     (swap! figwheel component/start)
-     (fig/cljs-repl (:figwheel-system @figwheel)))))
-
-(set-refresh-dirs "dev/server" "src/server" "src/shared")
 
 (defonce system (atom nil))
 
--- project.clj
+++ project.clj
@@ -12,7 +12,6 @@
                  [org.clojure/clojurescript "1.8.51"]
                  [org.omcljs/om "1.0.0-alpha41"]
                  [binaryage/devtools "0.5.2"]
-                 [figwheel-sidecar "0.5.9" :exclusions [ring/ring-core joda-time org.clojure/tools.reader]]
                  [com.cemerick/piggieback "0.2.1"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [juxt/dirwatch "0.2.3"]
@@ -42,8 +41,6 @@
                                :verbose              false
                                :recompile-dependents true
                                :source-map-timestamp true}}]}
-
-  :figwheel {:css-dirs ["resources/public/css"]}
 
   :repl-options {:init-ns          user
                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
